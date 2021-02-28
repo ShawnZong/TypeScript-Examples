f1adf1ac3866cc297a6f27a2606f74e7bb67c41a
shawnjstan <29206292+ShawnZong@users.noreply.github.com>
2021-02-28 02:28:03 +0200

    add more TypeAdditionalFields

--- 9.16/frontend/src/AddEntryModal/TypeAdditionalField.tsx
+++ 9.16/frontend/src/AddEntryModal/TypeAdditionalField.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 // Form
 import { Field } from "formik";
-import { NumberField } from "./FormField";
+import { NumberField, TextField } from "./FormField";
 
 export const TypeAdditionalField: React.FC<{ type: string }> = ({ type }) => {
   switch (type) {
@@ -15,6 +15,46 @@ export const TypeAdditionalField: React.FC<{ type: string }> = ({ type }) => {
           min={0}
           max={3}
         />
+      );
+    case "OccupationalHealthcare":
+      return (
+        <div>
+          <Field
+            label="Employer Name"
+            placeholder="Employer Name"
+            name="employerName"
+            component={TextField}
+          />
+          <Field
+            label="Sick Leave Start Date"
+            placeholder="YYYY-MM-DD"
+            name="sickLeaveStartDate"
+            component={TextField}
+          />
+          <Field
+            label="Sick Leave End Date"
+            placeholder="YYYY-MM-DD"
+            name="sickLeaveEndDate"
+            component={TextField}
+          />
+        </div>
+      );
+    case "Hospital":
+      return (
+        <div>
+          <Field
+            label="Discharge Date"
+            placeholder="YYYY-MM-DD"
+            name="dischargeDate"
+            component={TextField}
+          />
+          <Field
+            label="Discharge Criteria"
+            placeholder="Discharge Criteria"
+            name="dischargeCrieria"
+            component={TextField}
+          />
+        </div>
       );
     default:
       return null;
